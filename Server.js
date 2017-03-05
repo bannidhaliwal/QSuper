7a32eb0582fb3ab81d90a8302917b719737fd061
Banni <navjotsingh.dhaliwal@connect.qut.edu>
2017-03-05 20:59:35 +1000

    Node modules and Test page initialized

---
+++ Server.js
@@ -0,0 +1,15 @@
+//Require Socket.io for real time synchornisation
+//Require ejs for templating
+//Require express for serving files
+var PORT = 3000;
+var express = require ('express');
+var server = express();
+
+server.set("view engine","ejs");
+
+server.get('/',function(req,res){
+  res.render('test.ejs');
+});
+
+server.listen(PORT);
+console.log("This application is listening on" +PORT);
