8a2d20af6fc66e2ed79f361a8eea8fedcdf8a8ac
Banni <navjotsingh.dhaliwal@connect.qut.edu>
2017-03-09 02:26:40 +1000

    Templates Updated, Game Class

--- QSuperModules.js
+++ QSuperModules.js
@@ -6,31 +6,40 @@
 //Always Make sure the page names are lowercase (In
 //directory as well)
 var Router = function(request,response,server,mySqlObject){
-  var paths = {HomePage:"homepage",About:"about",PrivacyPolicy:"privacypolicy",GamePage:"gamepage",Login:"login"}; //Pages of Q-Super
+  var paths = {HomePage:"homepage",About:"about",PrivacyPolicy:"privacypolicy",GamePage:"gamepage",Login:"login",Register:"register",ContactUs:"contactus"}; //Pages of Q-Super
   var URL = request.originalUrl; //get URl
   if(request.method === "GET"){
     //remove the leading character before switching
     switch(sliceString(URL)){
       case paths["HomePage"]:{
-        response.render(paths["HomePage"]+".ejs");
+        response.render(paths["HomePage"]);
         break;
       }
       case paths["About"]:{
-        response.render(paths["About"]+".ejs");
+        response.render(paths["About"]);
         break;
       }
       case paths["PrivacyPolicy"]:{
-        response.render(paths["PrivacyPolicy"]+".ejs");
+        response.render(paths["PrivacyPolicy"]);
         break;
       }
       case paths["GamePage"]:{
-        response.render(paths["GamePage"]+".ejs");
+        response.render(paths["GamePage"]);
         break;
       }
       case paths["Login"]:{
         response.render(paths["Login"]);
         break;
       }
+      case paths["Register"]:{
+        response.render(paths["Register"]);
+        break;
+      }
+      case paths["ContactUs"]:{
+        response.render(paths["ContactUs"]);
+        break;
+      }
+
       case '/':{
         response.render(paths['HomePage']);
         break;
--- Server.js
+++ Server.js
@@ -1,20 +1,20 @@
 //This file is the main entry point for
-//Q_Super Project.
+//Q-Super Project.
 //@author: Banni
 var express = require ('express');
 var server = express();
 var bodyParser = require('body-parser');
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8080; //Let heroku decide which port to choose.
 var mySql = require('mysql');
-var localModules = require('./QSuperModules.js');
+var localModules = require('./QSuperModules.js');//Load the local modules.
 server.set("view engine","ejs");
 server.use(express.static('public'));
-server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.urlencoded({ extended: true })); //Not necessary ??
 //Middleware for routing
 server.use(function(req,res){
   localModules.Router(req,res,server,mySql);
 });
+//MySQL need to be pooled.
 localModules.MySqlConnection(mySql);
-
 server.listen(PORT);
 console.log("This application is listening on " +PORT);
