6a31e057cf600ab3240ae764164c93d71c812266
Banni <navjotsingh.dhaliwal@connect.qut.edu>
2017-03-14 05:42:24 +1000

    Code Refcatored

--- test/sampletest.js
+++ test/sampletest.js
@@ -2,21 +2,17 @@ var assert = require('assert');
 var GameBox = require('../lib/GameClass.js');
 
 describe('#CreateGameBox()', function() {
-  it('Should equal to 64 as number of total boxes in game.', function() {
-    assert.equal(64,GameBox.CreateGameBox(function(){}));
+  it('Should equal to 89 as we have last ID which is 88', function() {
+    assert.equal(89,GameBox.CreateGameBox(function(){}));
   });
 });
 
-describe('#GameBox(12)',function() {
-  it('X pos Should equal to 125 as ID is 12 means Gamebox is on column 2 in first row',function() {
-    var tempGameBox = new GameBox.GameBox(12);
-    assert.equal(125,tempGameBox.ReturnXPos());
-  });
-});
-
-describe('#GameBox(21)',function() {
-  it('Y pos Should equal to 125 as ID is 21 means Gamebox is on column 1 in second row',function() {
-    var tempGameBox = new GameBox.GameBox(21);
-    assert.equal(125,tempGameBox.ReturnYPos());
+describe('#getElementFromArray(21)',function() {
+  it('ID should be 21 and Token should be SUN',function() {
+    var gameBox = GameBox.CreateGameBox(function(){});
+    var tempGameBox = GameBox.ReturnSpecificElementFromArray(21);
+    tempGameBox.SetToken(GameBox.TOKENS.SUN);
+    assert.equal(21,tempGameBox.ReturnId());
+    assert.equal(GameBox.TOKENS.SUN,tempGameBox.ReturnToken());
   });
 });
