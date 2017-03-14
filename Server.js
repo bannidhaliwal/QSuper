c02a5fc21099757d83f7f666d1c94d643b119974
Banni <navjotsingh.dhaliwal@connect.qut.edu>
2017-03-14 19:29:00 +1000

    Code refactored and Socket.io included

--- Server.js
+++ Server.js
@@ -3,19 +3,30 @@
 //@author: Banni
 var express = require ('express');
 var server = express();
+var io = require('socket.io');
 var bodyParser = require('body-parser');
-var PORT = process.env.PORT || 8080; //Let heroku decide which port to choose.
 var mySql = require('mysql');
-var localModules = require('./lib/QSuperModules.js');//Load the local modules.
+var Routes = require('./lib/Routes.js');//Load the local modules.
+var SQL = require('./lib/SQL.js');
 var GameBox = require('./lib/GameClass.js');
+
+var PORT = process.env.PORT || 8080; //Let heroku decide which port to choose.
+
 server.set("view engine","ejs");
 server.use(express.static('public'));
-server.use(bodyParser.urlencoded({ extended: true })); //Not necessary ??
+server.use(bodyParser.urlencoded({ extended: true })); //Not necessary ?
 //Middleware for routing
 server.use(function(req,res){
-  localModules.Router(req,res,server,mySql);
+  Routes.Router(req,res,server,mySql);
 });
 //MySQL need to be pooled.
-localModules.MySqlConnection(mySql);
-server.listen(PORT);
+SQL.MySqlConnection(mySql);
+
+// Socket.io
+io = io.listen(server.listen(PORT));
+io.sockets.on('connection', function (socket) {
+    socket.emit("Hi");
+    GameBox.CreateGameBox(function(a){socket.emit("Game Initialized",{array : a});});
+});
+
 console.log("This application is listening on " +PORT);
