d62f13f0740f433665e6900fad4c554bae2d2b48
Banni <navjotsingh.dhaliwal@connect.qut.edu>
2017-04-05 01:40:20 +1000

    Logics completed and more tests added

--- test/SocketClassFunctionsTests.js
+++ test/SocketClassFunctionsTests.js
@@ -1,5 +1,10 @@
+//This class is for the testing purposes of the socket functions.
+//It will test all the logics and return values which are passed for
+//just testing purposes and will match them to the expected results.
+//Make sure after making any changes these tests pass.
 var assert = require('assert');
 var socketFunctions = require("../lib/SocketFunctions.js");
+var Constants = require('../lib/Constants.js');
 
 describe('#SocketFunctions.checkIfElementIsInFirstRow(yCoord)', function() {
   it('Should be true as the yCoord is = 1', function() {
@@ -39,20 +44,20 @@ describe('#SocketFunctions.checkIfTheElementIsInTheFirstColumn(xCoord)',function
 });
 
 describe('#SocketFunctions.checkIfElementCanHaveAllLegalMoves(id)',function(){
-  it('Should return false as the ID provided is 22 and the element can move in all four directions',function(){
-    assert.equal(false,socketFunctions.checkIfElementCanHaveAllLegalMoves("22"));
+  it('Should return true as the ID provided is 22 and the element can move in all four directions',function(){
+    assert.equal(true,socketFunctions.checkIfElementCanHaveAllLegalMoves("22"));
   });
-  it('Should return true as the ID provided is 11 and the element is in first row and column',function(){
-    assert.equal(true,socketFunctions.checkIfElementCanHaveAllLegalMoves("11"));
+  it('Should return false as the ID provided is 11 and the element is in first row and column',function(){
+    assert.equal(false,socketFunctions.checkIfElementCanHaveAllLegalMoves("11"));
   });
-  it('Should return true as the ID provided is 28 and the element is in last column hence cannot move forward',function(){
-    assert.equal(true,socketFunctions.checkIfElementCanHaveAllLegalMoves("28"));
+  it('Should return false as the ID provided is 28 and the element is in last column hence cannot move forward',function(){
+    assert.equal(false,socketFunctions.checkIfElementCanHaveAllLegalMoves("28"));
   });
-  it('Should return true as the ID provided is 21 and the element is in the first column',function(){
-    assert.equal(true,socketFunctions.checkIfElementCanHaveAllLegalMoves("21"));
+  it('Should return false as the ID provided is 21 and the element is in the first column',function(){
+    assert.equal(false,socketFunctions.checkIfElementCanHaveAllLegalMoves("21"));
   });
-  it('Should return false as the ID provided is 24 and the element can move in all directions',function(){
-    assert.equal(false,socketFunctions.checkIfElementCanHaveAllLegalMoves("24"));
+  it('Should return true as the ID provided is 24 and the element can move in all directions',function(){
+    assert.equal(true,socketFunctions.checkIfElementCanHaveAllLegalMoves("24"));
   });
 });
 
@@ -187,4 +192,86 @@ describe('#SocketFunctions.ifElementIsInLastRowAndFirstColumn',function(){
     assert.equal(false,socketFunctions.ifElementIsInTheLastRowAndFirstColumn("82"));
   });
 
+});
+
+describe("#SocketFunctions.DetermineThePositionOfClickedElement",function(){
+  it('Should return AL as the all movements are legal.',function(){
+    assert.equal(Constants().ALL_LEGAL,socketFunctions.DetermineThePositionOfClickedElement("27"));
+  });
+  it('Should return LE as the elements is on the left edge.',function(){
+    assert.equal(Constants().LEFT_EDGE,socketFunctions.DetermineThePositionOfClickedElement("11"));
+  });
+  it('Should return RE as the element is on the right edge.',function(){
+    assert.equal(Constants().RIGHT_EDGE,socketFunctions.DetermineThePositionOfClickedElement("18"));
+  });
+  it('Should return BLE as the element is on the bottom left edge.',function(){
+    assert.equal(Constants().BOTTOM_LEFT_EDGE,socketFunctions.DetermineThePositionOfClickedElement("81"));
+  });
+  it('Should return BRE as the element is on the bottom right edge.',function(){
+    assert.equal(Constants().BOTTOM_RIGHT_EDGE,socketFunctions.DetermineThePositionOfClickedElement("88"));
+  });
+  it('Should return BR as the element is in the bottom row.',function(){
+    assert.equal(Constants().BOTTOM_ROW,socketFunctions.DetermineThePositionOfClickedElement("85"));
+  });
+  it('Should return BR as the element is in the bottom row.',function(){
+    assert.equal(Constants().BOTTOM_ROW,socketFunctions.DetermineThePositionOfClickedElement("83"));
+  });
+  it('Should return UR as the element is in the upper row.',function(){
+    assert.equal(Constants().UPPER_ROW,socketFunctions.DetermineThePositionOfClickedElement("15"));
+  });
+  it('Should return UR as the element is in the upper row.',function(){
+    assert.equal(Constants().UPPER_ROW,socketFunctions.DetermineThePositionOfClickedElement("16"));
+  });
+  it('Should return RC as the element is in the right column.',function(){
+    assert.equal(Constants().RIGHT_COLUMN,socketFunctions.DetermineThePositionOfClickedElement("68"));
+  });
+  it('Should return LC as the element is in the left column.',function(){
+    assert.equal(Constants().LEFT_COLUMN,socketFunctions.DetermineThePositionOfClickedElement("61"));
+  });
+
+});
+
+describe("#SocketFunctions.AssignPositions",function(){
+  it('Should return 21,12 because the element is on the left edge and can just move to east and south only',function(){
+    assert.equal(21,socketFunctions.AssignPositions("",null,11,Constants().LEFT_EDGE)[0]);
+    assert.equal(12,socketFunctions.AssignPositions("",null,11,Constants().LEFT_EDGE)[1]);
+  });
+  it('Should return 28,17 as the element is on the right edge and only can move west and south only',function(){
+    assert.equal(28,socketFunctions.AssignPositions("",null,18,Constants().RIGHT_EDGE)[0]);
+    assert.equal(17,socketFunctions.AssignPositions("",null,18,Constants().RIGHT_EDGE)[1]);
+  });
+  it('Should return 25,14,16 as the element is in the first row and cannot move north only.',function(){
+    assert.equal(25,socketFunctions.AssignPositions("",null,15,Constants().UPPER_ROW)[0]);
+    assert.equal(14,socketFunctions.AssignPositions("",null,15,Constants().UPPER_ROW)[1]);
+    assert.equal(16,socketFunctions.AssignPositions("",null,15,Constants().UPPER_ROW)[2]);
+  });
+  it('Should return 87,78 as the element is in the Bottom right edge and can move north and west only.',function(){
+    assert.equal(78,socketFunctions.AssignPositions(null,null,88,Constants().BOTTOM_RIGHT_EDGE)[0]);
+    assert.equal(87,socketFunctions.AssignPositions(null,null,88,Constants().BOTTOM_RIGHT_EDGE)[1]);
+  });
+  it('Should return 82,71 as the element is in the Bottom left edge and can move east and north only.',function(){
+    assert.equal(71,socketFunctions.AssignPositions(null,null,81,Constants().BOTTOM_LEFT_EDGE)[0]);
+    assert.equal(82,socketFunctions.AssignPositions(null,null,81,Constants().BOTTOM_LEFT_EDGE)[1]);
+  });
+  it('Should return 75,86,84 as the element is in the last row and cannot move in south direction only',function(){
+    assert.equal(75,socketFunctions.AssignPositions(null,null,85,Constants().BOTTOM_ROW)[0]);
+    assert.equal(86,socketFunctions.AssignPositions(null,null,85,Constants().BOTTOM_ROW)[1]);
+    assert.equal(84,socketFunctions.AssignPositions(null,null,85,Constants().BOTTOM_ROW)[2]);
+  });
+  it('Should return 68,88,77 as the element is in the last column and cannot move in east direction only.',function(){
+    assert.equal(68,socketFunctions.AssignPositions(null,null,78,Constants().RIGHT_COLUMN)[0]);
+    assert.equal(88,socketFunctions.AssignPositions(null,null,78,Constants().RIGHT_COLUMN)[1]);
+    assert.equal(77,socketFunctions.AssignPositions(null,null,78,Constants().RIGHT_COLUMN)[2]);
+  });
+  it('Should return 61,81,72 as the element is in the left edge and cannot move in west direction only',function(){
+    assert.equal(61,socketFunctions.AssignPositions(null,null,71,Constants().LEFT_COLUMN)[0]);
+    assert.equal(81,socketFunctions.AssignPositions(null,null,71,Constants().LEFT_COLUMN)[1]);
+    assert.equal(72,socketFunctions.AssignPositions(null,null,71,Constants().LEFT_COLUMN)[2]);
+  });
+  it('Should should return 36,16,25,27 as the element is in the position where it can move in all directions.',function(){
+    assert.equal(36,socketFunctions.AssignPositions(null,null,26,Constants().ALL_LEGAL)[0]);
+    assert.equal(16,socketFunctions.AssignPositions(null,null,26,Constants().ALL_LEGAL)[1]);
+    assert.equal(27,socketFunctions.AssignPositions(null,null,26,Constants().ALL_LEGAL)[2]);
+    assert.equal(25,socketFunctions.AssignPositions(null,null,26,Constants().ALL_LEGAL)[3]);
+  });
 });
